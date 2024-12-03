80b08e269295f7f7092c3d38948c93603ffad977
CaptinNemrac <42428934+CaptinNemrac@users.noreply.github.com>
2024-12-03 11:11:00 -0500

    Add files via upload

---
+++ js/booking.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded", () => {
+    var HOME_BUTTON = document.getElementById("home")
+    var GUIDELINES_BUTTON = document.getElementById("guidelines")
+    var COLLEGE_BUTTONS = document.getElementsByClassName("college")
+    var RADIOS = document.getElementsByClassName("radio")
+
+    HOME_BUTTON.addEventListener("click", () => {
+        location.href = "./index.html";
+    })
+
+    GUIDELINES_BUTTON.addEventListener("click", () => {
+        location.href = "./guidelines.html";
+    })
+
+    for (let index = 0; index < COLLEGE_BUTTONS.length; index++) {
+        let radio = RADIOS[index]
+
+        COLLEGE_BUTTONS[index].addEventListener("click", () => {
+            for (let index2 = 0; index2 < RADIOS.length; index2++) {
+                if (index == index2) continue;
+                RADIOS[index2].checked = false;
+                if (RADIOS[index2].classList.contains("checked"))
+                    RADIOS[index2].classList.remove("checked")
+            }
+
+            radio.classList.toggle("checked")
+            console.log(radio.classList.contains("checked"))
+            radio.checked = radio.classList.contains("checked");
+        })
+    }
+})
--- js/index.js
+++ js/index.js
@@ -4,10 +4,14 @@ document.addEventListener("DOMContentLoaded", () => {
     var BOOK_BUTTON = document.getElementById("book")
     
     BOOKINGS_BUTTON.addEventListener("click", () => {
-        location.href = "/booking.html";
+        location.href = "./booking.html";
     })
 
     GUIDELINES_BUTTON.addEventListener("click", () => {
-        location.href = "/guidelines.html";
+        location.href = "./guidelines.html";
+    })
+
+    BOOK_BUTTON.addEventListener("click", () => {
+        location.href = "./booking.html";
     })
 })
